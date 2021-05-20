ba7ad72dce0390f7afa34668eac39a8c643c1889
Darío Candotti <dario.candotti@gmail.com>
2021-05-19 23:25:56 -0300

    how to create a particle system with javascript classes

--- script.js
+++ script.js
@@ -24,17 +24,55 @@ canvas.addEventListener("mousemove", event => {
 	mouse.y = event.y
 })
 
-function drawCircle () {
-	ctx.fillStyle = "blue"
-	ctx.beginPath()
-	ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2)
-	ctx.fill()
+class Particle {
+	constructor () {
+		//this.x = mouse.x
+		//this.y = mouse.y
+		this.x = Math.random() * canvas.width
+		this.y = Math.random() * canvas.height
+		this.size = Math.random() * 15 + 1
+		this.speedX = Math.random() * 3 - 1.5
+		this.speedY = Math.random() * 3 - 1.5
+	}
+
+	update () {
+		this.x += this.speedX
+		this.y += this.speedY
+		if (this.size > 0.2) this.size -= 0.1
+	}
+
+	draw () {
+		ctx.fillStyle = "blue"
+		ctx.beginPath()
+		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+		ctx.fill()
+	}
+}
+
+const particles = []
+
+function init () {
+	for (let i = 0; i < 100; i++) {
+		particles.push(new Particle())
+	}
+}
+
+function handleParticles () {
+	for (let i = 0; i < particles.length; i++) {
+		particles[i].update()
+		particles[i].draw()
+		if (particles[i].size < 0.3) {
+			particles.splice(i, 1)
+			i--
+		}
+	}
 }
 
 function animate () {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	drawCircle()
+	handleParticles()
 	requestAnimationFrame(animate)
 }
 
+init()
 animate()
