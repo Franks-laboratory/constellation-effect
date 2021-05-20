4db2551dbbb7d1b453b778fa22693fa21b770b56
Darío Candotti <dario.candotti@gmail.com>
2021-05-19 22:20:33 -0300

    how to set up canvas projects with plain vanilla javascript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "white"
+ctx.fillRect(10, 10, 150, 50)
