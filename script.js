2f90cbf14b19db149e8eacfe5ebb3e10e04ec327
Darío Candotti <dario.candotti@gmail.com>
2021-05-19 22:37:58 -0300

    drawing circle with arc method

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ canvas.height = window.innerHeight
 window.addEventListener("resize", () => {
 	canvas.width = window.innerWidth
 	canvas.height = window.innerHeight
-
-	ctx.fillStyle = "white"
-	ctx.fillRect(10, 10, 150, 50)
 })
 
-ctx.fillStyle = "white"
-ctx.fillRect(10, 10, 150, 50)
+ctx.fillStyle = "blue"
+ctx.strokeStyle = "red"
+ctx.lineWidth = 15
+ctx.beginPath()
+ctx.arc(100, 100, 50, 0, Math.PI * 2)
+ctx.stroke()
+ctx.fill()
