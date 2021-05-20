6e8bd368f2f0d20c8057378c07d522e3b8ead8b5
Darío Candotti <dario.candotti@gmail.com>
2021-05-19 22:50:29 -0300

    mouse interactivity with click and mousemove events

--- script.js
+++ script.js
@@ -9,10 +9,32 @@ window.addEventListener("resize", () => {
 	canvas.height = window.innerHeight
 })
 
-ctx.fillStyle = "blue"
-ctx.strokeStyle = "red"
-ctx.lineWidth = 15
-ctx.beginPath()
-ctx.arc(100, 100, 50, 0, Math.PI * 2)
-ctx.stroke()
-ctx.fill()
+const mouse = {
+	x: undefined,
+	y: undefined
+}
+
+canvas.addEventListener("click", event => {
+	mouse.x = event.x
+	mouse.y = event.y
+})
+
+canvas.addEventListener("mousemove", event => {
+	mouse.x = event.x
+	mouse.y = event.y
+})
+
+function drawCircle () {
+	ctx.fillStyle = "blue"
+	ctx.beginPath()
+	ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2)
+	ctx.fill()
+}
+
+function animate () {
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	drawCircle()
+	requestAnimationFrame(animate)
+}
+
+animate()
