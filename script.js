bba8c7e16954bbbc21b7ef037b139de24ae4cd42
Darío Candotti <dario.candotti@gmail.com>
2021-05-19 22:29:02 -0300

    how to use resize event listener to prevent stretching

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+window.addEventListener("resize", () => {
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
+
+	ctx.fillStyle = "white"
+	ctx.fillRect(10, 10, 150, 50)
+})
+
 ctx.fillStyle = "white"
 ctx.fillRect(10, 10, 150, 50)
